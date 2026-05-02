89406d2ef27238cc9656288cbb45968dc07b4b04
Wugrind <quentbr@gmail.com>
2026-05-02 12:35:23 +0200

    Setup working for deployment of a fastapi docker image with corresponding services and ingress access

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import redis
+import os
+
+app = FastAPI()
+
+redis_host = os.getenv("REDIS_HOST", "localhost")
+r = redis.Redis(host=redis_host, port=6379)
+
+@app.get("/")
+def read_root():
+    try:
+        count = r.incr("visits")
+    except:
+        count = "Redis not connected"
+    return {"visits": count}
